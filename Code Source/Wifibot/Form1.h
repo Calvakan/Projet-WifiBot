21eaf96845a363bc320b636962cc4b3321ee1b09
Ali Douiyek <alidouiyek@gmail.com>
2014-02-11 01:30:24 +0100

    Ancrages

--- Code Source/Wifibot/Form1.h
+++ Code Source/Wifibot/Form1.h
@@ -1,4 +1,3 @@
-///TEST Commit
 #pragma once
 #include "WifibotClient.h"
 #include "time.h"
@@ -102,9 +101,10 @@ namespace Wifibot {
 			// 
 			// groupBox1
 			// 
+			this->groupBox1->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
 			this->groupBox1->Controls->Add(this->rbSimulateur);
 			this->groupBox1->Controls->Add(this->rbRobot);
-			this->groupBox1->Location = System::Drawing::Point(12, 272);
+			this->groupBox1->Location = System::Drawing::Point(7, 340);
 			this->groupBox1->Name = L"groupBox1";
 			this->groupBox1->Size = System::Drawing::Size(186, 39);
 			this->groupBox1->TabIndex = 0;
@@ -138,7 +138,8 @@ namespace Wifibot {
 			// 
 			// btConnecter
 			// 
-			this->btConnecter->Location = System::Drawing::Point(28, 327);
+			this->btConnecter->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
+			this->btConnecter->Location = System::Drawing::Point(23, 395);
 			this->btConnecter->Name = L"btConnecter";
 			this->btConnecter->Size = System::Drawing::Size(150, 43);
 			this->btConnecter->TabIndex = 1;
@@ -150,7 +151,8 @@ namespace Wifibot {
 			// 
 			// btArriere
 			// 
-			this->btArriere->Location = System::Drawing::Point(338, 374);
+			this->btArriere->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
+			this->btArriere->Location = System::Drawing::Point(333, 442);
 			this->btArriere->Name = L"btArriere";
 			this->btArriere->RightToLeft = System::Windows::Forms::RightToLeft::Yes;
 			this->btArriere->Size = System::Drawing::Size(99, 43);
@@ -165,7 +167,8 @@ namespace Wifibot {
 			// 
 			// btStop
 			// 
-			this->btStop->Location = System::Drawing::Point(338, 328);
+			this->btStop->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
+			this->btStop->Location = System::Drawing::Point(333, 396);
 			this->btStop->Name = L"btStop";
 			this->btStop->Size = System::Drawing::Size(99, 43);
 			this->btStop->TabIndex = 4;
@@ -177,7 +180,8 @@ namespace Wifibot {
 			// 
 			// btAvant
 			// 
-			this->btAvant->Location = System::Drawing::Point(338, 282);
+			this->btAvant->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
+			this->btAvant->Location = System::Drawing::Point(333, 350);
 			this->btAvant->Name = L"btAvant";
 			this->btAvant->Size = System::Drawing::Size(99, 43);
 			this->btAvant->TabIndex = 5;
@@ -191,7 +195,8 @@ namespace Wifibot {
 			// 
 			// btGauche
 			// 
-			this->btGauche->Location = System::Drawing::Point(233, 328);
+			this->btGauche->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
+			this->btGauche->Location = System::Drawing::Point(228, 396);
 			this->btGauche->Name = L"btGauche";
 			this->btGauche->Size = System::Drawing::Size(99, 43);
 			this->btGauche->TabIndex = 6;
@@ -205,7 +210,8 @@ namespace Wifibot {
 			// 
 			// btDroite
 			// 
-			this->btDroite->Location = System::Drawing::Point(443, 328);
+			this->btDroite->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
+			this->btDroite->Location = System::Drawing::Point(438, 396);
 			this->btDroite->Name = L"btDroite";
 			this->btDroite->Size = System::Drawing::Size(99, 43);
 			this->btDroite->TabIndex = 7;
@@ -224,17 +230,21 @@ namespace Wifibot {
 			// 
 			// webBrowser1
 			// 
+			this->webBrowser1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
 			this->webBrowser1->Location = System::Drawing::Point(3, 2);
 			this->webBrowser1->MinimumSize = System::Drawing::Size(20, 20);
 			this->webBrowser1->Name = L"webBrowser1";
-			this->webBrowser1->Size = System::Drawing::Size(734, 264);
+			this->webBrowser1->Size = System::Drawing::Size(718, 332);
 			this->webBrowser1->TabIndex = 8;
 			this->webBrowser1->Url = (gcnew System::Uri(L"http://192.168.1.106", System::UriKind::Absolute));
 			// 
 			// lbMoteurGauche
 			// 
+			this->lbMoteurGauche->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
 			this->lbMoteurGauche->AutoSize = true;
-			this->lbMoteurGauche->Location = System::Drawing::Point(685, 336);
+			this->lbMoteurGauche->Location = System::Drawing::Point(680, 404);
 			this->lbMoteurGauche->Name = L"lbMoteurGauche";
 			this->lbMoteurGauche->Size = System::Drawing::Size(16, 13);
 			this->lbMoteurGauche->TabIndex = 9;
@@ -242,8 +252,9 @@ namespace Wifibot {
 			// 
 			// lbMoteurDroite
 			// 
+			this->lbMoteurDroite->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
 			this->lbMoteurDroite->AutoSize = true;
-			this->lbMoteurDroite->Location = System::Drawing::Point(685, 357);
+			this->lbMoteurDroite->Location = System::Drawing::Point(680, 425);
 			this->lbMoteurDroite->Name = L"lbMoteurDroite";
 			this->lbMoteurDroite->Size = System::Drawing::Size(16, 13);
 			this->lbMoteurDroite->TabIndex = 10;
@@ -251,8 +262,9 @@ namespace Wifibot {
 			// 
 			// lbBatterie
 			// 
+			this->lbBatterie->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
 			this->lbBatterie->AutoSize = true;
-			this->lbBatterie->Location = System::Drawing::Point(685, 316);
+			this->lbBatterie->Location = System::Drawing::Point(680, 384);
 			this->lbBatterie->Name = L"lbBatterie";
 			this->lbBatterie->Size = System::Drawing::Size(16, 13);
 			this->lbBatterie->TabIndex = 11;
@@ -260,8 +272,9 @@ namespace Wifibot {
 			// 
 			// lbIRGauche
 			// 
+			this->lbIRGauche->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
 			this->lbIRGauche->AutoSize = true;
-			this->lbIRGauche->Location = System::Drawing::Point(685, 377);
+			this->lbIRGauche->Location = System::Drawing::Point(680, 445);
 			this->lbIRGauche->Name = L"lbIRGauche";
 			this->lbIRGauche->Size = System::Drawing::Size(16, 13);
 			this->lbIRGauche->TabIndex = 12;
@@ -269,8 +282,9 @@ namespace Wifibot {
 			// 
 			// lbIRDroite
 			// 
+			this->lbIRDroite->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
 			this->lbIRDroite->AutoSize = true;
-			this->lbIRDroite->Location = System::Drawing::Point(685, 399);
+			this->lbIRDroite->Location = System::Drawing::Point(680, 467);
 			this->lbIRDroite->Name = L"lbIRDroite";
 			this->lbIRDroite->Size = System::Drawing::Size(16, 13);
 			this->lbIRDroite->TabIndex = 13;
@@ -278,8 +292,9 @@ namespace Wifibot {
 			// 
 			// label2
 			// 
+			this->label2->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(593, 317);
+			this->label2->Location = System::Drawing::Point(588, 385);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(46, 13);
 			this->label2->TabIndex = 15;
@@ -287,8 +302,9 @@ namespace Wifibot {
 			// 
 			// label3
 			// 
+			this->label3->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(593, 336);
+			this->label3->Location = System::Drawing::Point(588, 404);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(81, 13);
 			this->label3->TabIndex = 16;
@@ -296,8 +312,9 @@ namespace Wifibot {
 			// 
 			// label4
 			// 
+			this->label4->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(593, 357);
+			this->label4->Location = System::Drawing::Point(588, 425);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(65, 13);
 			this->label4->TabIndex = 17;
@@ -305,8 +322,9 @@ namespace Wifibot {
 			// 
 			// label5
 			// 
+			this->label5->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(593, 377);
+			this->label5->Location = System::Drawing::Point(588, 445);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(59, 13);
 			this->label5->TabIndex = 18;
@@ -314,8 +332,9 @@ namespace Wifibot {
 			// 
 			// label6
 			// 
+			this->label6->Anchor = System::Windows::Forms::AnchorStyles::Bottom;
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(593, 399);
+			this->label6->Location = System::Drawing::Point(588, 467);
 			this->label6->Name = L"label6";
 			this->label6->Size = System::Drawing::Size(43, 13);
 			this->label6->TabIndex = 19;
@@ -331,7 +350,7 @@ namespace Wifibot {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(743, 428);
+			this->ClientSize = System::Drawing::Size(724, 492);
 			this->Controls->Add(this->label6);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
@@ -350,6 +369,7 @@ namespace Wifibot {
 			this->Controls->Add(this->btArriere);
 			this->Controls->Add(this->btConnecter);
 			this->Controls->Add(this->groupBox1);
+			this->MinimumSize = System::Drawing::Size(730, 520);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
 			this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &Form1::Form1_KeyDown);
@@ -364,9 +384,9 @@ namespace Wifibot {
 	#pragma endregion Winform
 		
 
-		private: System::Void btConnecter_Click(System::Object^  sender, System::EventArgs^  e)
+		private: System::Void btConnecter_Click(System::Object^  sender, System::EventArgs^  e) // Comportement bizarre, à vérifier
 		{//Connecte ou déconnecte le client
-			
+				
 
 			if (connexionReussie)		//On est déjà connécté
 			{	//On arrête le robot puis on se déconnecte
